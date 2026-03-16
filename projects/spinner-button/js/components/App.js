a98e4aa9e04cd7c932b1fad342b8cab13344a555
Keys <keys14052002@gmail.com>
2026-03-16 12:04:47 +0000

    project(spinner-button): make the button disability dynamic

--- projects/spinner-button/js/components/App.js
+++ projects/spinner-button/js/components/App.js
@@ -3,5 +3,6 @@ import AppButton from './AppButton.js'
 export default {
     components: {
         'app-button': AppButton
+        
     }
 }
--- projects/spinner-button/js/components/AppButton.js
+++ projects/spinner-button/js/components/AppButton.js
@@ -1,5 +1,17 @@
 export default {
     template: `
-            <button class="bg-blue-600 px-5 py-3 text-white rounded hover:bg-blue-700 cursor-pointer px-g"><slot /></button> 
-    `
+            <button :class="bg-blue-600 px-5 py-3 text-white rounded hover:bg-blue-700 cursor-pointer px-g disabled:cursor-not-allowed" @click="disableButton" :disabled="buttonDisabledStatus"><slot /></button>
+    `,
+
+    data(){
+        return {
+            buttonDisabledStatus: false 
+        }
+    },
+
+    methods: {
+        disableButton() {
+            this.buttonDisabledStatus = true
+        }
+    }
 }
