2decede4589717d97e9bc5f947dad6ebdc38de9c
Keys <keys14052002@gmail.com>
2026-03-16 11:14:09 +0000

    project(spinner-button): style the button

--- projects/spinner-button/js/components/AppButton.js
+++ projects/spinner-button/js/components/AppButton.js
@@ -1,5 +1,5 @@
 export default {
     template: `
-        <button><slot /></button> 
+            <button class="bg-blue-600 px-5 py-3 text-white rounded hover:bg-blue-700 cursor-pointer px-g"><slot /></button> 
     `
 }
