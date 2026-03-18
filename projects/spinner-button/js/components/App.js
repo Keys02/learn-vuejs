0af33850125621ed7024a359cf0f473cd6894759
Keys <keys14052002@gmail.com>
2026-03-18 13:34:19 +0000

    project(spinner-button): make the button clickable only once

--- projects/spinner-button/js/components/App.js
+++ projects/spinner-button/js/components/App.js
@@ -3,6 +3,5 @@ import AppButton from './AppButton.js'
 export default {
     components: {
         'app-button': AppButton
-        
     }
 }
--- projects/spinner-button/js/components/AppButton.js
+++ projects/spinner-button/js/components/AppButton.js
@@ -1,6 +1,15 @@
 export default {
     template: `
-            <button :class="bg-blue-600 px-5 py-3 text-white rounded hover:bg-blue-700 cursor-pointer px-g disabled:cursor-not-allowed" @click="disableButton" :disabled="buttonDisabledStatus"><slot /></button>
+            <button 
+                :class="{
+                    'bg-blue-600 px-5 py-3 text-white rounded hover:bg-blue-700 cursor-pointer px-g disabled:cursor-not-allowed': true,
+                    'is-loading': buttonDisabledStatus
+                }" 
+                @click="disableButton"
+                :disabled="buttonDisabledStatus"
+            >
+                <slot />
+            </button>
     `,
 
     data(){
@@ -13,5 +22,5 @@ export default {
         disableButton() {
             this.buttonDisabledStatus = true
         }
-    }
+   }
 }
