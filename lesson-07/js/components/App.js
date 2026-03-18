be45ee26d9e5f5bbcb61f69087d73dc82d10a865
Keys <keys14052002@gmail.com>
2026-03-18 13:58:25 +0000

    chore: put every project in it's own directory

--- lesson-07/js/components/App.js
+++ lesson-07/js/components/App.js
@@ -1,59 +1,12 @@
 import AppButton from "./AppButton.js";
+import Assignments from "./Assignments.js"
 
 export default {
-    template: `
-        <section class="mt-2" v-show="inProgressAssignments.length">
-            <h1 class="font-bold mb-2">In Progress</h1>
-            <ul>
-                <li 
-                    v-for="assignment in inProgressAssignments" 
-                    :key="assignment.id"
-                >
-                    <label>
-                        {{ assignment.name }}
-                        <input type="checkbox" v-model="assignment.complete">
-                    </label>
-                </li>
-            </ul>
-        </section>
-
-        <section v-show="completedAssignments.length" class="mt-2">
-            <h1 class="font-bold">Completed Assignments</h1>
-            <ul>
-                <li 
-                    v-for="assignment in completedAssignments" 
-                    :key="assignment.id"
-                >
-                    <label class="line-through">
-                        {{ assignment.name }}
-                        <input type="checkbox" v-model="assignment.complete">
-                    </label>
-                </li>
-            </ul>
-        </section>
-    `,
-
     components: {
-        'app-button': AppButton
+        Assignments
     },
 
-    data() {
-        return {
-            assignments: [
-                {id: 1, name: 'Clean my room', complete: false},
-                {id: 2, name: 'Walk the dog', complete: false},
-                {id: 3, name: 'Do my homework', complete: false},
-            ]
-        }
-    },
-
-    computed: {
-        inProgressAssignments() {
-            return this.assignments.filter(a => ! a.complete)
-        },
-
-        completedAssignments() {
-            return this.assignments.filter(a => a.complete) 
-        }
-    }
+    template: `
+        <assignments></assignments>
+    `
 }
---
+++ lesson-07/js/components/Assignments.js
@@ -0,0 +1,54 @@
+export default {
+    template: `
+        <section class="mt-2" v-show="inProgressAssignments.length">
+            <h1 class="font-bold mb-2">In Progress</h1>
+            <ul>
+                <li 
+                    v-for="assignment in inProgressAssignments" 
+                    :key="assignment.id"
+                >
+                    <label>
+                        {{ assignment.name }}
+                        <input type="checkbox" v-model="assignment.complete">
+                    </label>
+                </li>
+            </ul>
+        </section>
+
+        <section v-show="completedAssignments.length" class="mt-2">
+            <h1 class="font-bold">Completed Assignments</h1>
+            <ul>
+                <li 
+                    v-for="assignment in completedAssignments" 
+                    :key="assignment.id"
+                >
+                    <label class="line-through">
+                        {{ assignment.name }}
+                        <input type="checkbox" v-model="assignment.complete">
+                    </label>
+                </li>
+            </ul>
+        </section>
+    `,
+
+    data() {
+        return {
+            assignments: [
+                {id: 1, name: 'Clean my room', complete: false},
+                {id: 2, name: 'Walk the dog', complete: false},
+                {id: 3, name: 'Do my homework', complete: false},
+            ]
+        }
+    },
+
+    computed: {
+        inProgressAssignments() {
+            return this.assignments.filter(a => ! a.complete)
+        },
+
+        completedAssignments() {
+            return this.assignments.filter(a => a.complete) 
+        }
+    }
+
+}
