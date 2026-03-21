1474e99079cb705eec929268888c2876e7e9a03f
Keys <keys14052002@gmail.com>
2026-03-21 01:06:47 +0000

    explore: using forms

---
+++ lesson-08/js/components/Assignments.js
@@ -0,0 +1,77 @@
+export default {
+    template: `
+        <section class="mt-2" v-show="inProgressAssignments.length">
+            <h1 class="font-bold mb-2">In Progress</h1>
+            <ul class="border divide-y">
+                <li 
+                    v-for="assignment in inProgressAssignments" 
+                    :key="assignment.id"
+                >
+                    <label class="p-2 flex justify-between">
+                        {{ assignment.name }}
+                        <input type="checkbox" v-model="assignment.complete" class="ml-3">
+                    </label>
+                </li>
+            </ul>
+        </section>
+
+        <section class="space-y-6" v-show="completedAssignments.length">
+            <h1 class="font-bold mb-2 mt-3">Completed Assignments</h1>
+            <ul class="border divide-y">
+                <li 
+                    v-for="assignment in completedAssignments" 
+                    :key="assignment.id"
+                >
+                    <label class="line-through p-2 flex justify-between">
+                        {{ assignment.name }}
+                        <input type="checkbox" v-model="assignment.complete" class="ml-3">
+                    </label>
+                </li>
+            </ul>
+        </section>
+        <form @submit.prevent="add" class="mt-3">
+            <div class="border p-2">
+                <input v-model="newAssignment" class="bg-white text-black p-2" type="text" placeholder="New assignment..."/>
+                <button class="ml-2 bg-white text-black rounded p-2" type="submit">Add</button>
+            </div>
+        </form>
+
+    `,
+
+    data() {
+        return {
+            assignments: [
+                {id: 1, name: 'Clean my room', complete: false},
+                {id: 2, name: 'Walk the dog', complete: false},
+                {id: 3, name: 'Do my homework', complete: false},
+            ],
+
+            newAssignment: ''
+        }
+    },
+
+    computed: {
+        inProgressAssignments() {
+            return this.assignments.filter(a => ! a.complete)
+        },
+
+        completedAssignments() {
+            return this.assignments.filter(a => a.complete) 
+        }
+    },
+
+    methods: {
+        add(e) {
+            this.assignments.push(
+                {
+                    id: this.assignments.length + 1,
+                    name: this.newAssignment,
+                    completed: false
+
+                }
+            )
+            this.newAssignment = ''
+        }
+    }
+
+}
