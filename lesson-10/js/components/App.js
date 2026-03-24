ee350996f497871705bfc4253d4c6bef988338d6
Keys <keys14052002@gmail.com>
2026-03-24 23:43:31 +0000

    explore: add a length to the assignments

---
+++ lesson-10/js/components/App.js
@@ -0,0 +1,13 @@
+import AppButton from "./AppButton.js";
+import Assignments from "./Assignments.js"
+import CreateAssignment from "./CreateAssignment.js"
+
+export default {
+    components: {
+        Assignments,
+    },
+
+    template: `
+        <assignments></assignments>
+    `
+}
